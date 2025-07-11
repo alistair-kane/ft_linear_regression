f0081f1bd9517420dd6d57c94883b3de8a62f5cd
alistair kane <alistair.kane94@gmail.com>
2025-07-11 17:13:00 +0200

    added README

--- linear_regression/linearregression/src/main.rs
+++ linear_regression/linearregression/src/main.rs
@@ -2,12 +2,15 @@ use std::{error::Error, io, process};
 
 fn example() -> Result<(), Box<dyn Error>> {
     let mut file = csv::Reader::from_reader(io::stdin());
-    for record in file.records()
-    {
-        let check = record?;
-        println!("Line {:?}", check);
+    for record in file.records() {
+        let line = record?;
+        println!("Line {:?}", line);
     }
     Ok(())
+}
+
+fn estimatePrice(mileage: f32, theta0: f32, theta1: f32) -> f32 {
+    return theta0 + (theta1 * mileage)
 }
 
 fn main() {
